5aa49dc64d0c7b1209420d01093a97774a96c720
Jisnaaj <102594767+Jisnaaj@users.noreply.github.com>
2022-07-17 18:39:58 +0530

    changed localhost

--- src/app/serive.service.ts
+++ src/app/serive.service.ts
@@ -19,11 +19,11 @@ export class SeriveService {
 
  onsubmit(user:any){
 
-    return this.http.post<any>("http://localhost:3002/login",user)
+    return this.http.post<any>("https://library-backendjisna.herokuapp.com/login",user)
     }
 
     getbooks(){
-      return  this.http.get("http://localhost:3002/book");
+      return  this.http.get("https://library-backendjisna.herokuapp.com/book");
       };
 
       
@@ -32,25 +32,25 @@ export class SeriveService {
     }
 
     addbooks=(item:any)=>{
-      return  this.http.post("http://localhost:3002/addbook",{item})
+      return  this.http.post("https://library-backendjisna.herokuapp.com/addbook",{item})
      .subscribe((data=>{console.log(data)}));
     }
     
     getProduct(id:any){
-      return this.http.get("http://localhost:3002/"+id);
+      return this.http.get("https://library-backendjisna.herokuapp.com/"+id);
     }
 
     editProduct(product:any)
   {
     console.log('client update')
-    return this.http.put("http://localhost:3002/update",product)
+    return this.http.put("https://library-backendjisna.herokuapp.com/update",product)
     .subscribe(data =>{console.log(data)})
   }
 
   deleteProduct(id:any)
   {
 
-    return this.http.delete("http://localhost:3002/remove/"+id)
+    return this.http.delete("https://library-backendjisna.herokuapp.com/remove/"+id)
 
   }
   }
